5db2d1a44390acdb7cff248bfce28749415f060c
Matheusck10 <107003553+matheuserickputo@users.noreply.github.com>
2024-08-13 09:57:53 -0300

    m2

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector('.caixaPrincipal')
+const caixaPerguntas = document.querySelector('.caixaPerguntas')
+const caixaAlternativas = document.querySelector('.caixaAlternativa')
+const caixaresultado = document.querySelector('.caixaResultado')
+const textoResultado = document.querySelector('.textoResultado')
+
+const perguntas =[
+{
+    enunciado:"no ambito social, você prefere",
+    alternativas:
+    [
+        {
+            texto:"você tem muita sorte durante toda a vida",
+            afirmação:"mas 25% do racismo almenta",
+        },
+        {
+            texto:"você deve tirar a vida de 25 inocentes",
+            afirmação:"mas o crime no mundo diminui em 255"
+        }
+    ]
+}
+]
